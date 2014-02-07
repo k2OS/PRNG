82778554220b585366a7e37fa6394951f8003414
René Mikkelsen <rene.mikkelsen@gmail.com>
2014-02-07 19:02:32 +0100

    Converted to TinyGPS

--- PRNG.ino
+++ PRNG.ino
@@ -1,7 +1,7 @@
 #include <LiquidCrystal_I2C.h>
-#include <TinyGPS++.h>
+#include <TinyGPS.h>
 #include <SoftwareSerial.h>
-#include <EEPROM.h>
+// #include <EEPROM.h>
 #include <Time.h>
 #include <Wire.h>
 
@@ -11,7 +11,15 @@ LiquidCrystal_I2C lcd(0x20,4,5,6,0,1,2,3,7,NEGATIVE);
 #include <MemoryFree.h>
 #include <pgmStrToRAM.h>
 */
-TinyGPSPlus gps;
+
+/* GPS-related variables */
+float flat, flon;
+unsigned long age, date, time;
+int Year;
+byte Month, Day, Hour, Minute, Second;
+
+
+TinyGPS gps;
 // rx = 6 (yellow), tx = 7 (white)
 // vcc = black, gnd = green
 SoftwareSerial nss(7,6);
@@ -20,7 +28,7 @@ SoftwareSerial nss(7,6);
 static const float COORDINATE_LAT = 55;
 static const float COORDINATE_LON = 12;
 
-time_t rightnow; // save now() in this - also used to seed the PRNG
+time_t codetime; // save now() in this - also used to seed the PRNG
 
 //  tællere til nedtælling - vi starter på 30 minutter (1800 sekunder)
 // formel: currentInterval = defaultInterval*distance(m)*hastighed(km/t) 
@@ -42,40 +50,36 @@ void setup() {
 void loop() {
     // stuff the gps-object with data
     feedgps();
+    gps.f_get_position(&flat, &flon, &age);
 
-    if (gps.location.isValid()) {
+     // this might not be needed as we need to talk to the GPS 'all the time' even after first fix
+    //updatedatetime(); // runs for 1000ms to make sure we have date-time correct
+
+    if (age < 1000) {
       // I only want to update the display once per interval
-      int speed = (int)gps.speed.kmph(); // a sort of rounding
+      int speed = (int)gps.f_speed_kmph(); // a sort of rounding
       if (millis()-previousMillis >= interval) {
         lcd.setCursor(0,0);
         lcd.print("                ");
         lcd.setCursor(0,0);
-        lcd.print(gps.location.lat(), 6);
+        lcd.print(flat, 6);
 
         lcd.setCursor(0,1);
         lcd.print("                ");
         lcd.setCursor(0,1);
-        lcd.println(gps.location.lng(), 6);
+        lcd.println(flon, 6);
 
-        lcd.setCursor(10,0); lcd.print("     ");
-        lcd.setCursor(10,0); 
+        lcd.setCursor(12,0); lcd.print("    ");
+        lcd.setCursor(12,0); 
         if (speed > 9) { 
           lcd.print("  "); 
         }  else if (speed > 99) { lcd.print(" "); } 
-
         lcd.print(speed);
-    //    Serial.print("LAT="); Serial.println(gps.location.lat(), 6);
-    //    Serial.print("LNG="); Serial.println(gps.location.lng(), 6);
 
         previousMillis = millis();
-        // Set the internal time to time received from the GPS - if time is valid
-        if (gps.time.isValid()) {
-          // settime(hour, minute, second, day, month, year)
-          setTime(gps.time.hour(), gps.time.minute(), gps.time.second(), gps.date.day(), gps.date.month(), gps.date.year()); 
-          rightnow = now(); // setting the "Unix"-time - I will evaluate if I want to create a code later
-        }
       }
         /*
+        // do stuff with speed and distance here to calculate countdown for code-retrieval
       randomSeed(i); // "now"
       randNumber = random(999,9999);
       Serial.println(randNumber);
@@ -103,3 +107,10 @@ static bool feedgps()
   return false;
 }
 
+
+void updatedatetime() {
+         start = millis();
+         while (millis() - start < 1000) {
+            feedgps();
+         }
+}
